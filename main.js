9892918e9851d05f3e8bc26d4b05d544ee125926
Rémi Bezeux <rbezeux@gmail.com>
2023-05-29 09:35:45 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// Main threads object
+var threads={};
+// On each mouse scroll
+document.onmousewheel = function(){
+  // If channel entry does not exist...
+if (!Object.keys(threads).includes(document.title.split('|')[1].trim())) {
+    threads[document.title.split('|')[1].trim()] = [];
+}
+
+    // List all threads
+    document.querySelectorAll('[class*="threadName-"]').forEach(function(u, i){
+    // Put the thread in the channel array
+     !threads[document.title.split('|')[1].trim()].includes(u.textContent) ? threads[document.title.split('|')[1].trim()].push(u.textContent) : '';
+    
+
+        
+})
+}
